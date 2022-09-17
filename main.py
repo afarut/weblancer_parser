ac12c13830b6b78dddf437f556b100786ea13ce4
afarut <renat030205@ya.ru>
2022-09-17 12:14:38 +0300

    add link

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import requests
 
 jobs = get_jobs()
 if jobs:
-	text = "\n".join(jobs)
+	text = ""
+	for key, val in jobs.items():
+		text += f"[{key}]({val})\n"
 	url = f"https://api.telegram.org/bot{API_KEY}/sendMessage"
 	data = {"chat_id": CHANNEL_ID, "text": text, "parse_mode": "Markdown"}
 	response = requests.post(url, data=data)
--- parser.py
+++ parser.py
@@ -8,8 +8,13 @@ def get_jobs():
 	html = s.get("https://www.weblancer.net/jobs/veb-programmirovanie-31/").text
 	soup = BeautifulSoup (html, 'html.parser')
 	containers = soup.find_all("div", class_="row click_container-link set_href")
-	new_jobs = set(map(lambda x: x.find("a", class_="text-bold click_target show_visited").text, containers))
-	jobs = list(new_jobs.copy())
+	new_jobs = {}
+	for container in containers:
+		row = container.find("a", class_="text-bold click_target show_visited")
+		new_jobs[row.text] = "https://www.weblancer.net" + row.attrs["href"]
+
+	jobs = new_jobs.copy()
+	new_jobs = set(new_jobs.keys())
 
 	with open(BASE_DIR / 'jobs.json') as json_file:
 	    old_jobs = set(json.load(json_file))
@@ -18,5 +23,8 @@ def get_jobs():
 	new_jobs = list(new_jobs)
 
 	with open(BASE_DIR / 'jobs.json', 'w') as outfile:
-	    json.dump(jobs, outfile)
-	return new_jobs
+	    json.dump(list(jobs.keys()), outfile)
+	data = {}
+	for title in new_jobs:
+		data[title] = jobs[title]
+	return data
